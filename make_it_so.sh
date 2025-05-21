50df19e57a8272502905b09a746ee9db279a92be
karpob <bryan.m.karpowicz@nasa.gov>
2025-05-20 23:57:25 -0400

    uncomment mkdir

--- make_it_so.sh
+++ make_it_so.sh
@@ -16,7 +16,7 @@ elif [[ ${SEL} == 'apple_intel' ]]; then
     source ~/miniconda3/bin/activate
     conda init --all
 elif [[ ${SEL} == 'linux' ]]; then
-    #mkdir -p ~/miniconda3
+    mkdir -p ~/miniconda3
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
     bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
     rm ~/miniconda3/miniconda.sh
