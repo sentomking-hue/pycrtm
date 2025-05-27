04c71777844817839604ef62bdf4bb7c41c112be
Bryan M. Karpowicz <bryan.m.karpowicz@nasa.gov>
2025-05-27 12:34:03 -0400

    additional features added to make_it_so.sh

--- make_it_so.sh
+++ make_it_so.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export SEL=$1
-
+set --
 if [[ ${SEL} == 'apple_silicon' ]]; then
     mkdir -p ~/miniconda3
     curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
@@ -22,25 +22,34 @@ elif [[ ${SEL} == 'linux' ]]; then
     rm ~/miniconda3/miniconda.sh
     source ~/miniconda3/bin/activate
     conda init --all
-elif [[ ${SEL} == 'skip' ]]; then
+elif [[ ${SEL} == 'skip_install' || ${SEL} == 'skip_create' || ${SEL} == 'skip_crtm_download' ]]; then
+
     echo "Skipping miniconda install. Already installed."    
     if [[ -f "$HOME/miniconda3/bin/activate" ]]; then
         source $HOME/miniconda3/bin/activate
     fi
+
 else
-    echo "Unknown platform selected:${1}"
+    echo "Unknown option selected:${1}"
 	exit 1
 fi
 
-# Add conda-forge, necessary to get netcdf-fortran
-conda config --add channels conda-forge
 
 export CONDA_VENV='pycrtm'
-conda create --name ${CONDA_VENV} python scikit-build h5py netcdf4 gfortran libnetcdf netcdf-fortran meson cmake git git-lfs matplotlib
-conda activate ${CONDA_VENV}
-conda init
-echo $CONDA_PREFIX
 
+if [ ${SEL} == 'skip_create' ] || [ ${SEL} == 'skip_crtm_download' ]; then
+  echo "using current conda environment: ${CONDA_VENV}"
+  echo "Skipping conda create."
+  conda activate ${CONDA_VENV}
+else 
+  # Add conda-forge, necessary to get netcdf-fortran
+  conda config --add channels conda-forge
+
+  conda create --name ${CONDA_VENV} python scikit-build h5py netcdf4 gfortran libnetcdf netcdf-fortran meson cmake matplotlib wget
+  conda activate ${CONDA_VENV}
+  conda init
+  echo $CONDA_PREFIX
+fi
 
 
 export PWDOLD=${PWD}
@@ -48,26 +57,41 @@ export CHECKOUT_PATH="${PWD}/ext"
 export PYCRTM_PATH="${PWD}"
 export CRTM_VERSION="3.x"
 
-# checkout and buildtest CRTM
+# checkout and build/test CRTM
 mkdir ${CHECKOUT_PATH}
 echo ${CHECKOUT_PATH}
 cd ${CHECKOUT_PATH}
 echo ${PWD}
 
-if [[ ${CRTM_VERSION} == "2.x" ]]; then
-  git clone https://github.com/ecmwf/ecbuild
-  export PATH=${PWD}/ecbuild/bin:$PATH
-  git lfs install --skip-smudge
-  git clone https://github.com/JCSDA/crtm/
+if [ ${CRTM_VERSION} == "2.x" ] && [ ${SEL} != 'skip_crtm_download' ]; then
+  wget https://github.com/ecmwf/ecbuild/archive/refs/tags/3.10.0.tar.gz
+  tar -xvf 3.10.0.tar.gz
+  export PATH=${PWD}/ecbuild-3.10.0/bin:$PATH
+
+  wget https://github.com/JCSDA/crtm/archive/refs/tags/v2.4.1-jedi.1.tar.gz
+  tar -xvf v2.4.1-jedi.1.tar.gz
+  if [ -d "${CHECKOUT_PATH}/crtm" ]; then
+    rm -r crtm
+  fi
+  mv crtm-2.4.1-jedi.1 crtm   
   cd crtm
-  git checkout v2.4.1-jedi.1
-  git lfs pull
+elif [[ ${CRTM_VERSION} == "2.x" ]]; then
+  echo "Skipping CRTM download."
+  export PATH=${PWD}/ecbuild-3.10.0/bin:$PATH
+  cd crtm
 fi
 
-if [[ ${CRTM_VERSION} == "3.x" ]]; then
-  git clone https://github.com/JCSDA/CRTMv3.git
+if [ ${CRTM_VERSION} == "3.x" ] && [ ${SEL} != 'skip_crtm_download' ]; then
+  wget https://github.com/JCSDA/CRTMv3/archive/refs/tags/v3.1.1+build1.tar.gz
+  tar -xvf v3.1.1+build1.tar.gz
+  if [ -d "${CHECKOUT_PATH}/CRTMv3" ]; then
+    rm -r CRTMv3
+  fi
+  mv CRTMv3-3.1.1-build1 CRTMv3
   cd CRTMv3
-  git checkout v3.1.1+build1
+elif [[ ${CRTM_VERSION} == "3.x" ]]; then
+  echo "Skipping CRTM download."
+  cd CRTMv3
 fi
 
 export FPIC="-fPIC"
@@ -92,8 +116,7 @@ elif [[ ! "$!FORTRANFLAGS" =~ (^|[[:space:]])"${FPIC}"($|[[:space:]]) ]]; then
 else
   echo "Flag '${FPIC}' is already set in FORTRANFLAGS."
 fi
-#conda list
-if [[ ${CRTM_VERSION} == "2.x" ]]; then
+if [ ${CRTM_VERSION} == "2.x" ] && [ ${SEL} != 'skip_crtm_download' ]; then
   chmod +x Get_CRTM_Binary_Files.sh
   ./Get_CRTM_Binary_Files.sh
 fi
@@ -102,6 +125,12 @@ mkdir build
 cd build
 
 if [[ ${CRTM_VERSION} == "2.x" ]]; then
+  if [[ -d "${CHECKOUT_PATH}/crtm/fix_REL-2.4.1_20221109/" ]]; then
+    cd ..
+    rm -rf build
+    mkdir build
+    cd build
+  fi
   ecbuild ../
   make -j8 
   ctest -j8
@@ -111,8 +140,15 @@ if [[ ${CRTM_VERSION} == "2.x" ]]; then
 fi
  
 
-
 if [[ ${CRTM_VERSION} == "3.x" ]]; then
+  if [[ -d "${CHECKOUT_PATH}/CRTMv3/build/test_data/3.1.1/fix_REL-3.1.1.2/" ]]; then
+    mv ${CHECKOUT_PATH}/CRTMv3/build/test_data/ ../
+    cd ..
+    rm -rf build
+    mkdir build
+    mv test_data build
+    cd build
+  fi
   cmake -DBUILD_SHARED_LIBS=OFF ../
   make -j8 
   make install 
@@ -121,8 +157,7 @@ fi
  
 
 cd ${PYCRTM_PATH}
-#git clone git@github.com:JCSDA/pycrtm.git
-#git checkout feature/prepCRTMv3
+
 rm setup.cfg
 printf "[Setup]\n">>setup.cfg
 printf "# Specify the location of the crtm install\n">>setup.cfg 
@@ -154,7 +189,7 @@ pip install .
 cd testCases
 if [[ $SEL == 'apple_intel' ]]; then
     export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:${CHECKOUT_PATH}/CRTMv3/build/lib/" 
-elif [[ $SEL == 'skip' ]]; then
+elif [[ ${SEL:0:4} == 'skip' ]]; then
     export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:${CHECKOUT_PATH}/CRTMv3/build/lib/" 
 fi
 python3 test_atms.py 
